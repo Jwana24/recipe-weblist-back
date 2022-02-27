535938c6940e59f7e0224b4e0cb3ff0b4dc6c0b5
Johanna <johanna.detrieux@gmail.com>
2022-02-27 16:24:06 +0100

    Update the display of the data recipe name Add a route to get all recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,12 +3,30 @@ const router = express.Router();
 const connection = require('../connection');
 
 router.post('/', (req, res) => {
-    const dataRecipe = req.body;
+    const dataRecipe = {
+        recipe_image: req.body.image,
+        recipe_time: req.body.time,
+        recipe_name: req.body.name,
+        recipe_text: req.body.text,
+        recipe_author: req.body.author,
+        recipe_date_creation: req.body.date,
+    };
     connection.query('INSERT INTO recipe SET ?', [dataRecipe], (err, _) => {
         if (err) {
             res.status(500).send(err);
         } else {
             res.status(201).send('La recette a bien été créée');
+        }
+    });
+});
+
+router.get('/', (req, res) => {
+    const idRecipe = req.params.id;
+    connection.query('SELECT * FROM recipe', [], (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).json(results);
         }
     });
 });
